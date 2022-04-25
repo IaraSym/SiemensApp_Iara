490ca8e3a970ceba50a91a7b095e1f03a3d53676
IaraSym <iara.1996@hotmail.com>
2022-04-25 05:55:13 +0100

    datas ok

--- src/app/pages/project/project.component.ts
+++ src/app/pages/project/project.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {DataSource} from '@angular/cdk/collections';
+import {Observable, ReplaySubject} from 'rxjs';
+
+export interface PeriodicElement {
+  name: string;
+  startdate: string;
+  enddate: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {name: 'First Job', startdate: '10-05-2015', enddate: '15-11-2015'},
+  
+];
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
-export class ProjectComponent implements OnInit {
+export class ProjectComponent {
+  displayedColumns: string[] = ['name', 'startdate', 'enddate', 'action'];
+  dataToDisplay = [...ELEMENT_DATA];
 
-  constructor() { }
+  dataSource = new ExampleDataSource(this.dataToDisplay);
 
-  ngOnInit(): void {
+  addData() {
+    const randomElementIndex = Math.floor(Math.random() * ELEMENT_DATA.length);
+    this.dataToDisplay = [...this.dataToDisplay, ELEMENT_DATA[randomElementIndex]];
+    this.dataSource.setData(this.dataToDisplay);
+  }
+
+  removeData() {
+    this.dataToDisplay = this.dataToDisplay.slice(0, -1);
+    this.dataSource.setData(this.dataToDisplay);
+  }
+}
+
+class ExampleDataSource extends DataSource<PeriodicElement> {
+  private _dataStream = new ReplaySubject<PeriodicElement[]>();
+
+  constructor(initialData: PeriodicElement[]) {
+    super();
+    this.setData(initialData);
+  }
+
+  connect(): Observable<PeriodicElement[]> {
+    return this._dataStream;
+  }
+
+  disconnect() {}
+
+  setData(data: PeriodicElement[]) {
+    this._dataStream.next(data);
   }
 
 }
