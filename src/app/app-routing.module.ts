c95a1b5c9c3ca28dafe6e2b9539a486f054b9ecc
IaraSym <iara.1996@hotmail.com>
2022-04-25 03:09:26 +0100

    paginas ok

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { InfoComponent } from './pages/info/info.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+const routes: Routes = [
+
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'homepage',
+  },
+  {
+    path: 'homepage',
+    component: HomepageComponent,     
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'info',
+    component: InfoComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
